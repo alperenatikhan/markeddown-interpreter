97746380bb996ca3338ab6d554697248070e493c
alperenatikhan <alperenmyung@gmail.com>
2021-03-21 20:26:10 +0100

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import React from "react";
 import "./style.css";
+import marked from "marked"
 
-const marked = require("marked");
 
-export default function App() {
+
+class App extends React.Component{
+  constructor(props){
+    super();
+    this.state= {
+      input:'',
+      markdown:''
+    };
+    this.handleChange = this.handleChange.bind(this);
+  
+  }
+handleChange(event){
+  this.setState({input: event.target.value})
+  const marked = require("marked");
+  const html = marked(this.state.input);
+  this.setState({markdown: html });
+} 
+
+  render(){
   return (
     <div>
       <div className="col" id="markdown-container">
-        <textarea name="input-area" id="editor" cols="" rows="" />
+        <textarea 
+        name="input-area" 
+        id="editor" 
+        onChange={this.handleChange.bind(this)}
+        />
       </div>
       <div className="col">
         <div id="preview" className="output-area">
-          <p id="display"> </p>
+           {this.state.markdown}
         </div>
       </div>
     </div>
   );
 }
+};
+
+export default App;
